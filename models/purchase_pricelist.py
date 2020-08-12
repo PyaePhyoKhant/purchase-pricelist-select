f25f470c865e549b6d752ca8ac36fdb0a8d37b5a
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2020-08-12 22:07:00 +0630

    purchase.pricelist.item

--- models/purchase_pricelist.py
+++ models/purchase_pricelist.py
@@ -7,3 +7,11 @@ class PurchasePricelist(models.Model):
 
     country_group_ids = fields.Many2many('res.country.group', 'res_country_group_purchase_pricelist_rel',
                                          'purchase_pricelist_id', 'res_country_group_id', string='Country Groups')
+    item_ids = fields.One2many('purchase.pricelist.item')
+
+
+class PricelistItem(models.Model):
+    _name = 'purchase.pricelist.item'
+    _inherit = "product.pricelist.item"
+
+    pricelist_id = fields.Many2one('purchase.pricelist')
