59d3ac6fe83543e3b8aa6399274cd91f83affde6
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2020-08-13 22:38:12 +0630

    add main feature: unit price according to pricelist

--- models/purchase_order_inherit.py
+++ models/purchase_order_inherit.py
@@ -14,6 +14,16 @@ class PurchaseOrderLine(models.Model):
     def _onchange_quantity(self):
         """
         `_onchange_quantity` method set unit price
-        super call this method first and set price form purchase pricelist
+        super call this method first and then set price form purchase pricelist
         """
-        super()._onchange_quantity()
+        if self.product_id:
+            # super call method first so that vendor pricelist still works correctly even without purchase pricelist
+            super()._onchange_quantity()
+
+            # search by variant first and then if not found by template
+            price_list_item = self.order_id.vendor_pl_id.item_ids.filtered(lambda x: x.product_id == self.product_id)
+            if not price_list_item:
+                price_list_item = self.order_id.vendor_pl_id.item_ids.filtered(lambda x: x.product_tmpl_id == self.product_id.product_tmpl_id)
+
+            if price_list_item:
+                self.price_unit = price_list_item.fixed_price
