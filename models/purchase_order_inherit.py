4187411fe9622e8057829571a3cde748fd4b58e3
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2020-08-13 22:05:58 +0630

    [WIP] inherit method that set unit price

--- models/purchase_order_inherit.py
+++ models/purchase_order_inherit.py
@@ -5,3 +5,15 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     vendor_pl_id = fields.Many2one('purchase.pricelist', 'Pricelist')
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        """
+        `_onchange_quantity` method set unit price
+        super call this method first and set price form purchase pricelist
+        """
+        super()._onchange_quantity()
